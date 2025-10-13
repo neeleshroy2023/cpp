57de2879920cd7259647aa6bcb03fb283404aba9
Neelesh Roy <128451760+neeleshroy2023@users.noreply.github.com>
2025-10-13 10:13:18 +0000

    more sorts

---
+++ python/mentorship/quick_sort.py
@@ -0,0 +1,18 @@
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i = i + 1
+            (arr[i], arr[j]) = (arr[j], arr[i])
+    (arr[i+1], arr[high]) = (arr[high], arr[i+1])
+    return i+1
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pivot = partition(arr, low, high)
+        quick_sort(arr, low, pivot - 1)
+        quick_sort(arr, pivot + 1, high)
+    return arr
+array = [8, 3, 1, 7, 0, 10, 2]
+print(quick_sort(array, 0, len(array) - 1))
